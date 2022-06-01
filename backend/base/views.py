6b1c292bd822d91574a44a8f156e0ba5ff4bc5be
japurva <japurva1@gmail.com>
2022-06-01 19:08:10 +0530

    Adding Frontend

---
+++ backend/base/views.py
@@ -0,0 +1,27 @@
+import imp
+from json import JSONDecodeError
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializers import ProductSerializer
+
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    return Response('Hello')
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
